8b93709a465beefa1842b60f883181822d1e0010
Asaf <asafebgi@gmail.com>
2024-10-10 09:15:22 -0700

    added midi parser lib + extension boilerplate

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,82 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "midi-viewer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('midi-viewer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MIDI Viewer!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerCustomEditorProvider(
+      'midiViewer.customEditor',
+      new MidiViewerCustomEditorProvider(context)
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class MidiViewerCustomEditorProvider
+  implements vscode.CustomTextEditorProvider
+{
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveCustomTextEditor(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel,
+    token: vscode.CancellationToken
+  ): void {
+    webviewPanel.webview.options = { enableScripts: true };
+    webviewPanel.webview.html = this.getHtmlForWebview(webviewPanel.webview);
+
+    const updateWebView = () => {
+      const text = document.getText();
+      webviewPanel.webview.postMessage({ type: 'update', text });
+    };
+
+    const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(
+      (e) => {
+        if (e.document.uri.toString() === document.uri.toString()) {
+          updateWebView();
+        }
+      }
+    );
+
+    webviewPanel.onDidDispose(() => {
+      changeDocumentSubscription.dispose();
+    });
+
+    updateWebView();
+  }
+
+  private getHtmlForWebview(webview: vscode.Webview): string {
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MIDI Viewer</title>
+        <style>
+          canvas { width: 100%; height: 100%; }
+        </style>
+      </head>
+      <body>
+        <canvas id="pianoRoll"></canvas>
+        <script>
+          const canvas = document.getElementById('pianoRoll');
+          const ctx = canvas.getContext('2d');
+
+          // Placeholder drawing
+          ctx.fillStyle = 'black';
+          ctx.fillRect(10, 10, 100, 50);
+
+			window.addEventListener('message', event => {
+			const message = event.data;
+			if (message.type === 'update') {
+				const midiData = new Uint8Array(message.text.split(','));
+				const midiFile = new MIDIFile(midiData);
+
+				const tracks = midiFile.getTrackEvents();
+				console.log('MIDI tracks:', tracks);
+
+				// TODO: Visualize the notes as a piano roll
+			}
+			});
+        </script>
+      </body>
+      </html>`;
+  }
+}
